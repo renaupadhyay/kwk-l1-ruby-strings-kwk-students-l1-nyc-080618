25c8dd2ee3e22e68a7e8b5de787ee8a38258751d
renaupadhyay <renaupadhyay01@gmail.com>
2018-08-07 13:26:52 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,15 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+def Invitation
+  name = Hermione
+  puts "Dear #{name},
+ 
+You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
+ 
+Sincerely,
+ 
+Harry Potter"
+end
+
+Invitation
